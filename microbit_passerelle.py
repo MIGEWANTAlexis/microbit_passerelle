7d472eed0c30dba4662986f2704b191b14277550
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-12-02 10:46:39 +0100

    fix: add comment

--- microbit_passerelle.py
+++ microbit_passerelle.py
@@ -2,15 +2,21 @@ from microbit import *
 import radio
 import tp3
 
+'''
+ * variables for script
+'''
 uart.init(115200)
 radio.config(group=2)
 radio.on()
 tp3 = tp3.RadioProtocol(1)
 
+'''
+ * main programme
+'''
 while True:
-    message = tp3.receivePacket(radio.receive_bytes())
-    msgUartStr = uart.read()
+    message = tp3.receivePacket(radio.receive_bytes())  # data from another microbit by radio frequency through the network protocol
+    msgUartStr = uart.read()                            # data per serial port
     if msgUartStr:
-        tp3.sendPacket(str(msgUartStr), 2)
+        tp3.sendPacket(str(msgUartStr), 2)              # send configuration (TL or LT)
     if message:
-        print(message)
+        print(message)                                  # send data by serial to the gateway
