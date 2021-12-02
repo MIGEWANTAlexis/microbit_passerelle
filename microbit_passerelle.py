e631d641ef82390a5223a9089e4bc197af0fa24e
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-12-02 10:55:01 +0100

    fix: code optim

--- microbit_passerelle.py
+++ microbit_passerelle.py
@@ -5,18 +5,22 @@ import tp3
 '''
  * variables for script
 '''
+radioProtocol = tp3.RadioProtocol(1)
+
+'''
+ * init comm
+'''
 uart.init(115200)
-radio.config(group=2)
+radio.config(group = 2)
 radio.on()
-tp3 = tp3.RadioProtocol(1)
 
 '''
  * main programme
 '''
 while True:
-    message = tp3.receivePacket(radio.receive_bytes())  # data from another microbit by radio frequency through the network protocol
+    message = radioProtocol.receivePacket(radio.receive_bytes())  # data from another microbit by radio frequency through the network protocol
     msgUartStr = uart.read()                            # data per serial port
     if msgUartStr:
-        tp3.sendPacket(str(msgUartStr), 2)              # send configuration (TL or LT)
+        radioProtocol.sendPacket(str(msgUartStr), 2)              # send configuration (TL or LT)
     if message:
         print(message)                                  # send data by serial to the gateway
