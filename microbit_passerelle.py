a37e78a5489f0f85cfc09153a11fed2fd5ea5e92
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-12-02 13:21:52 +0100

    fix: code optim

--- microbit_passerelle.py
+++ microbit_passerelle.py
@@ -18,9 +18,9 @@ radio.on()
  * main programme
 '''
 while True:
-    message = radioProtocol.receivePacket(radio.receive_bytes())  # data from another microbit by radio frequency through the network protocol
-    msgUartStr = uart.read()                            # data per serial port
+    message = radioProtocol.receivePacket(radio.receive_bytes())    # data from another microbit by radio frequency through the network protocol
+    msgUartStr = uart.read()                                        # data per serial port
     if msgUartStr:
-        radioProtocol.sendPacket(str(msgUartStr), 2)              # send configuration (TL or LT)
+        radioProtocol.sendPacket(str(msgUartStr), 2)                # send configuration (TL or LT)
     if message:
-        print(message)                                  # send data by serial to the gateway
+        print(message)                                              # send data by serial to the gateway
