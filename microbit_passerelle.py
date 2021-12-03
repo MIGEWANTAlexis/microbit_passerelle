5b16fa55a6dbe3607d5a38adea929a7ca39b3bc7
Alexis MIGEWANT <alexis.migewant@cpe.fr>
2021-12-03 18:09:37 +0100

    fix: syntax

--- microbit_passerelle.py
+++ microbit_passerelle.py
@@ -19,7 +19,7 @@ radio.on()
 '''
 while True:
     message = radioProtocol.receivePacket(radio.receive_bytes())    # data from another microbit by radio frequency through the network protocol
-    msgUartStr = uart.read()                                       # data per serial port
+    msgUartStr = uart.read()                                        # data per serial port
     if msgUartStr:
         radioProtocol.sendPacket(str(msgUartStr), 2)                # send configuration (TL or LT)
     if message:
